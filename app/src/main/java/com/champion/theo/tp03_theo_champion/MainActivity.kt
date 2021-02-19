dcb8624cb95550d0f8332efa6c78cf5ae03c3d5c
Théo <tochampion38@gmail.com>
2021-02-19 10:12:43 +0100

    🎉 Init project

---
+++ app/src/main/java/com/champion/theo/tp03_theo_champion/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.champion.theo.tp03_theo_champion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
